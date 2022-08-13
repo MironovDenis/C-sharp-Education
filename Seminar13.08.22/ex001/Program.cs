35a0eb53f8f7711e10566e8f4fd65afa9705cb36
Denis <mironov_dens@mail.ru>
2022-08-13 23:30:09 +0900

    Семинар 13.08.22

---
+++ Seminar13.08.22/ex001/Program.cs
@@ -0,0 +1,28 @@
+﻿int x = new Random().Next(-10, 10);
+int y = new Random().Next(-10, 10);
+Console.WriteLine($"Координата х = {x}");
+Console.WriteLine($"Координата y = {y}");
+
+while(x != 0 | y != 0)
+{
+    if(x > 0 && y > 0)
+    {
+        Console.WriteLine("Первая четверть"); 
+           }   
+
+    if(x < 0 && y > 0)
+    {
+        Console.WriteLine("Вторая четверть");
+            }
+
+    if(x < 0 && y < 0)
+    {
+        Console.WriteLine("Третья четверть");
+           }
+
+    if(x > 0 && y < 0)
+    {
+        Console.WriteLine("Четвертая четверть");
+            }
+break;
+}
---
+++ Seminar13.08.22/ex002/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1) -> 5,09 
+A (7,-5); B (1,-1) -> 7,21
+*/ 
+
+Random rnd = new Random();
+double x1 = rnd.Next(-100, 100)/10d;
+Console.WriteLine($"x1= {x1}");
+double y1 = rnd.Next(-100, 100)/10d;
+Console.WriteLine($"y1= {y1}");
+double x2 = rnd.Next(-100, 100)/10d;
+Console.WriteLine($"x2= {x2}");
+double y2 = rnd.Next(-100, 100)/10d;
+Console.WriteLine($"y2= {y2}");
+double dist = 0;
+
+dist = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine($"Расстояние между двумя точками на плоскости равно {Math.Round(dist, 3)}");
+
+
+/* Random rnd = new Random();
+            double d =rnd.Next(-1000, 1000)/100d;
+            Console.WriteLine(d);*/ 
