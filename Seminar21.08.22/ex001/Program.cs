a5a805e72b31305a53c94857a2176bd066610636
Denis <mironov_dens@mail.ru>
2022-08-25 22:35:54 +0900

    Lesson013

---
+++ Seminar21.08.22/ex001/Program.cs
@@ -0,0 +1,33 @@
+﻿//Показать двумерный массив размером m×n заполненный целыми числами
+
+int[,] Fill(int n, int m)
+{
+    int[,] table = new int[n, m];
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i, j] = new Random().Next(1, 25);
+        }
+    }
+    return table;
+}
+
+int[,] Print(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(table[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+
+    return table;
+}
+
+Fill(5, 4);
+Print(Fill(5, 4));
+
